467adc739002537f8e9cc5f0e68a8a1716f17391
diego bordeu <dbordeu@uc.cl>
2022-02-24 10:58:54 -0300

    wip

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from invoice_cession_feasibility import GetInvoiceFeasibility
 
 checker = GetInvoiceFeasibility()
 response = checker.check_cession_feasibility(
-    company_rut='76340335-1',
-    invoice_folio='544',
-    dte_type='33',
+    company_rut='xxxxxx-y',
+    invoice_folio='xxx',
+    dte_type='33', # 33 o 34
 )
 print(response)
